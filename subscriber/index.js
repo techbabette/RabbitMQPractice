394be43c68610eb863b16305d6eb9ad6ac8e99cd
techbabette <marinakrsticrf@gmail.com>
2024-07-05 17:28:18 +0200

    [FEAT] Working subscriber

---
+++ subscriber/index.js
@@ -0,0 +1,27 @@
+var amqp = require('amqplib/callback_api');
+
+amqp.connect('amqp://rabbitmq', function(error0, connection) {
+    if (error0) {
+      throw error0;
+    }
+    connection.createChannel(function(error1, channel) {
+      if (error1) {
+        throw error1;
+      }
+      var queue = 'hello';
+  
+      channel.assertQueue(queue, {
+        durable: false
+      });
+
+      console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", queue);
+      channel.consume(queue, handleHelloQueue, 
+      {
+          noAck: true
+      });
+    });
+  });
+
+  function handleHelloQueue(message){
+    console.log(" [x] Received a message: %s", message.content.toString());
+  }
